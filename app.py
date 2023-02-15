aea71064881a0481b0ee32c0336105a005cc6eab
Matthew Baer <matt.baer77@gmail.com>
2023-02-15 00:27:46 -0500

    users, add user - complete

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
@@ -19,6 +19,34 @@ connect_db(app)
 
 @app.route('/')
 def root():
-    """"""
+    """Shows a list of all users"""
 
-    return '<h1>CONNECTED</h1>'
+    return redirect('/users')
+
+@app.route('/users')
+def users():
+    """Shows a list of all users"""
+
+    users = User.query.all()
+
+    return render_template('users.html', users=users)
+
+@app.route('/add-user')
+def add_user_form():
+    """Shows a form to add a user"""
+
+    return render_template('add-user.html')
+
+@app.route('/add-user', methods=["POST"])
+def add_user_post():
+    """"Accepts form input, Adds a user to the database"""
+
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    img_url = request.form["img_url"]
+
+    new_user = User(first_name=first_name, last_name=last_name, img_url=img_url)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('/users')
